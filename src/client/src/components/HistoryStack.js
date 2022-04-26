97738e8e13d5e5cf863e501751cbe0ca479e7871
buditato <datajanoe@gmail.com>
2022-04-26 16:38:29 +0700

    styling - ngagaya

--- src/client/src/components/HistoryStack.js
+++ src/client/src/components/HistoryStack.js
@@ -3,60 +3,76 @@ import {Box, HStack, IconButton, VStack, Text, StackDivider, Spacer, Accordion,
     AccordionItem,
     AccordionButton,
     AccordionPanel,
-    AccordionIcon} from '@chakra-ui/react'
+    AccordionIcon,
+    Heading} from '@chakra-ui/react'
 import {MdExpandMore} from 'react-icons/md'
 
 function HistoryStack() {
     const history = [
         {
             id: 1,
-            body: 'epi',
-            status: "bucin kronis"
+            nama: 'epi',
+            penyakit: "bucin kronis",
+            persentase: '100%',
+            status: 'positive'
         },
         {
             id: 2,
-            body: 'anca',
-            status: 'kurang afeksi'
+            nama: 'anca',
+            penyakit: 'kurang afeksi',
+            persentase: '80%',
+            status: 'positive'
         },
         {
             id: 3,
-            body: 'budi',
-            status: 'kasep maksimal'
+            nama: 'budi',
+            penyakit: 'kasep maksimal',
+            persentase: '1000%',
+            status: 'positive'
         },
     ]
     return(
-        // <VStack
-        //     divider={<StackDivider />}
-        //     borderColor='gray.200'
-        //     borderWidth='2px'
-        //     p = '4'
-        //     borderRadius='lg'
-        //     w='100%'
-        //     maxW={{base: '90vww', sm: '80vw', md: '50vw', lg: '40vw'}}
-        //     alignItems='stretch'
-        // >
-        //     {history.map(item => (
-        //         <HStack key={item.id}>
-        //             <Text>{item.body}</Text>
-        //             <Spacer/>
-        //             <IconButton icon={<MdExpandMore/>}/>
-        //         </HStack>
-
-        //     ))}
-        // </VStack>
-        <Accordion defaultIndex={[0]} allowMultiple>
+        <Accordion defaultIndex={[0]} allowMultiple 
+        divider={<StackDivider />}
+        borderColor='gray.200'
+        borderWidth='1px'
+        p = '2'
+        borderRadius='lg'
+        w='100%'
+        maxW={{base: '90vww', sm: '80vw', md: '50vw', lg: '40vw'}}
+        alignItems='stretch'
+        backgroundColor='#91ACCA'
+        >
             {history.map(item => (
-            <AccordionItem key={item.id}>
+            <AccordionItem key={item.id} 
+            borderWidth='1px'
+            borderRadius='lg'
+            p = '2px'
+            mb= '10px'
+            backgroundColor='white'
+            >
                 <h2>
-                    <AccordionButton>
+                    <AccordionButton _expanded={{ bg: '#012B39', color: 'white' }} borderRadius='lg'>
                         <Box flex='1' textAlign='left'>
-                            {item.body}
+                            <Heading size= 'md'>
+                                {item.nama}
+                            </Heading>
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                 </h2>
                 <AccordionPanel pb={4}>
-                    {item.status}
+                    <VStack>
+                        <Heading size= 'base'>
+                            {item.penyakit}
+                        </Heading>
+                        <Heading size= 'base'>
+                            {item.persentase}
+                        </Heading>
+                        <Heading size= 'base'>
+                            {item.status}
+                        </Heading>
+                    </VStack>
                 </AccordionPanel>
             </AccordionItem>
             ))}
